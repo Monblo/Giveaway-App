82cbe579beed19c29dca6970bfa58470515a6541
Monblo <bloniarz@live.com>
2021-08-15 19:08:59 +0200

    footer form correction

--- src/components/Footer/Form.js
+++ src/components/Footer/Form.js
@@ -3,75 +3,73 @@ import {FooterButtonStyled} from "../Button/Button.styles";
 import {postForm} from "./fetch_api";
 
 const Form = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [nameError, setNameError] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [msgError, setMsgError] = useState('');
-    const [success, setSuccess] = useState('');
+    const [contactData, setContactData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+    const [error, setError] = useState({
+        nameError: '',
+        emailError: '',
+        msgError: '',
+        successError: '',
+        noError: ''
+    });
 
-    const nameChange = (e) => {
-        const tempName = e.target.value
-        setName(tempName)
-    };
-
-    const emailChange = (e) => {
-        const tempEmail = e.target.value
-        setEmail(tempEmail)
-    };
-
-    const messageChange = (e) => {
-        const tempMessage = e.target.value
-        setMessage(tempMessage)
+    const handleMessage = (e) => {
+        const name = e.target.name;
+        setContactData(prev => {
+            return {...prev, [name]: e.target.value}
+        })
     };
 
     //form validation
     const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (/\s/.test(name)){setNameError('Podane imię jest nieprawidłowe!')
-        } else {
-            setNameError(null)
+        e.preventDefault()
+        const errorsTmp = {
+            nameError: null,
+            emailError: null,
+            msgError: null,
+            successError: null
         }
 
-        if (message.length < 120){setMsgError('Wiadomość musi mieć conajmniej 120 znaków!')
-        } else {
-            setMsgError(null)
+        if (/\s/.test(contactData.name)) {
+            errorsTmp.nameError = 'Podane imię jest nieprawidłowe!'
+        }
+
+        if (contactData.message.length < 120) {
+            errorsTmp.msgError = 'Wiadomość musi mieć conajmniej 120 znaków!'
         }
 
         const re = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-        if (re.test(email)){setEmailError(null)
-        } else {
-            setEmailError('Podany email jest nieprawidłowy!')
+        if (re.test(contactData.email) === false) {
+            errorsTmp.emailError = 'Podany email jest nieprawidłowy!'
         }
 
-        // if (nameError == null && emailError == null && msgError == null){
-        //
-        //
-        // }
+        if (contactData.nameError === null && contactData.emailError === null && contactData.msgError === null) {
+            errorsTmp.successError = 'Wiadomość została wysłana! Wkrótce się skontaktujemy.'
+        }
+
+        setError(errorsTmp)
     };
 
-    //display success text & post data to endpoint
+    //send post data to endpoint
     useEffect(() => {
-        if (nameError == null && emailError == null && msgError == null){
-            setSuccess('Wiadomość została wysłana! Wkrótce się skontaktujemy.');
-            const formData = {
-                name: name,
-                email: email,
-                message: message
-            };
-            postForm(formData)
-            setName('');
-            setMessage('');
-            setEmail('');
-        } else {setSuccess(null)}
-    },[nameError, emailError, msgError]);
+        const contactTmp = {
+            name: '',
+            email: '',
+            message: ''
+        }
+        if (error.successError === null) {
+            postForm(contactData)
+            setContactData(contactTmp)
+        }
+    }, [error.successError]);
 
     const errorStyle = {
-        color:'red',
-        fontSize:'0.75rem',
-        fontWeight:'700'
+        color: 'red',
+        fontSize: '0.75rem',
+        fontWeight: '700'
     };
 
     const style = {
@@ -80,32 +78,29 @@ const Form = () => {
 
     return (
         <>
-            {success && <p style={{color:'green'}} className={'success'}>{success}</p>}
-        <form onSubmit={handleSubmit}>
-            <div className={'form__info'}>
-                <div className={'input__name'}>
-                    <label>Wpisz swoje imię</label>
-                    {nameError ? <><input type='text' name='name' placeholder='Monika' value={name} style={style} onChange={nameChange}/>
-                        <p style={errorStyle}>{nameError}</p>
-                        </> : <input type='text' name='name' placeholder='Monika' value={name} onChange={nameChange}/>}
+            {error.successError && <p style={{color: 'green'}} className={'success'}>{error.successError}</p>}
+            <form onSubmit={handleSubmit}>
+                <div className={'form__info'}>
+                    <div className={'input__name'}>
+                        <label>Wpisz swoje imię</label>
+                        <input type='text' name='name' placeholder='Monika' value={contactData.name}
+                               onChange={handleMessage} style={error.nameError ? style : {}}/>
+                        {error.nameError && <p style={errorStyle}>{error.nameError}</p>}
+                    </div>
+                    <div>
+                        <label>Wpisz swój email</label>
+                        <input type='text' name='email' placeholder='abc@xyz.pl' value={contactData.email}
+                               onChange={handleMessage} style={error.emailError ? style : {}}/>
+                        {error.emailError && <p style={errorStyle}>{error.emailError}</p>}
+                    </div>
                 </div>
-                <div>
-                    <label>Wpisz swój email</label>
-                    {emailError ? <><input type='text' name='email' placeholder='abc@xyz.pl' value={email} style={style} onChange={emailChange}/>
-                        <p style={errorStyle}>{emailError}</p>
-                    </> : <input type='text' name='email' placeholder='abc@xyz.pl' value={email} onChange={emailChange}/>}
-                </div>
-            </div>
-            <label>Wpisz swoją wiadomość</label>
-            {msgError ? <><textarea name='text' className={'input__text'} value={message} style={style} onChange={messageChange}
-                                    placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.'/>
-            <p style={errorStyle}>{msgError}</p>
-            </> : <textarea name='text' className={'input__text'} value={message} onChange={messageChange}
-                            placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.'/>}
-            {/*<LinkStyled to='/logowanie'>*/}
-            <FooterButtonStyled type='submit' className={'form__button'} >Wyślij</FooterButtonStyled>
-            {/*</LinkStyled>*/}
-        </form>
+                <label>Wpisz swoją wiadomość</label>
+                <textarea name='text' className={'input__text'} value={contactData.message}
+                          onChange={handleMessage} style={error.msgError ? style : {}}
+                          placeholder='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.'/>
+                {error.msgError && <p style={errorStyle}>{error.msgError}</p>}
+                <FooterButtonStyled type='submit' className={'form__button'}>Wyślij</FooterButtonStyled>
+            </form>
         </>
     );
 };
